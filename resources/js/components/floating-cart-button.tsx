330c9575aaf44510d6ef56e4cbf45597c020135e
Harlexander <dunkwualex6@gmail.com>
2025-12-09 00:06:24 +0100

    changes

--- resources/js/components/floating-cart-button.tsx
+++ resources/js/components/floating-cart-button.tsx
@@ -13,7 +13,7 @@ export default function FloatingCartButton({ onClick }: { onClick: () => void })
         onClick={onClick}
         className="relative h-12 rounded-full bg-[#A67C5B] px-6 text-white shadow-lg shadow-[#A67C5B]/30 hover:bg-orange-600 w-full text-lg transition-transform duration-300 ease-out hover:scale-105"
       >
-        <ShoppingCart className="mr-2 size-6" />Proceed to Booking
+        <ShoppingCart className="mr-2 size-6" />Proceed to Checkout
         {itemsCount > 0 && (
           <span className="absolute -right-2 -top-2 grid h-6 w-6 place-items-center rounded-full bg-white text-sm font-bold text-orange-600 shadow">
             {itemsCount}
